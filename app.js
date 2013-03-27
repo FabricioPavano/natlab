a69a349aee1d37e4016234d7f0179c40188b54a7
Fabricio Pavano <faripavano@gmail.com>
2013-03-27 10:41:23 -0430

    primeras horas de trabajo

--- app.js
+++ app.js
@@ -4,16 +4,47 @@ Ext.application({
     name: 'NL',
     appFolder: 'app',
 
-    controllers: [
-        'Productos'
-    ],
+    controllers: ['Principal'],
     
     launch: function() {
         Ext.create('Ext.container.Viewport', {
-            layout: 'fit',
-            items: {
-                
-            }
-        });
-    }
+            layout: 'border',
+            padding: '0 150 0 150', //esto le da el efecto centrado a la aplicacion
+            items: [
+            {
+                region:'west',
+                title:'NatLab',
+                flex:0.3,
+                layout:{
+                    type: 'accordion',
+                    titleCollapse: true,
+                    animate: true,
+                },
+                items:[{
+                    title:'Productos',
+                    layout:'anchor',
+                    items:[{
+                        xtype:'button',
+                        anchor:'100%',
+                        text:'<span class="letra-grande">Listar Productos</span>',
+                        scale:'large'
+                    }]
+                },
+                {
+                    title:'Clientes'
+                },
+                {
+                    title:'Facturacion'
+                },
+                {
+                    title:'Estadisticas'
+                }]
+            },
+            {
+                region: 'center',
+                title: 'centro'
+            }]
+
+        })
+    }    
 });
---
+++ app/controller/Principal.js
@@ -0,0 +1,11 @@
+//tiene como objetivo controlar aspectos generales de la aplicacion
+Ext.define('NL.controller.Principal', {
+
+    extend: 'Ext.app.Controller',
+    
+    views:[],
+
+    init: function() {
+        console.log('contorlador creado satisfactoriamente')
+    },
+});
